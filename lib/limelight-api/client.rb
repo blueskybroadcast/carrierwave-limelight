78e4433d3ddb16bd427386724bc1788055f282ca
Elochai <buu9992@gmail.com>
2017-06-27 13:03:54 +0300

    Change jsonrpc request "id" parameter from float to string
    - plus add some logging for troubleshooting

--- lib/limelight-api/client.rb
+++ lib/limelight-api/client.rb
@@ -1,4 +1,4 @@
-require "rest-client"
+require 'rest-client'
 
 module LimelightApi
   class Client
@@ -8,13 +8,15 @@ module LimelightApi
       @jsonrpc_endpoint = 'http://global.llp.lldns.net:8080/jsonrpc2'
     end
 
-    def upload file_path, file_base_name = nil, directory = nil, tries=2
+    def upload file_path, file_base_name = nil, directory = nil, tries = 2
       begin
-        file_base_name = file_path.split("/").last unless file_base_name
+        file_base_name = file_path.split('/').last unless file_base_name
         @last_uploaded_file = RestClient.post( @upload_endpoint, get_file(file_path), upload_headers(file_base_name, directory) )
         @last_uploaded_file.headers[:x_agile_path]
       rescue RestClient::Forbidden => e
-        puts e.response.headers
+        Rails.logger.warn "Limelight Upload Response Forbidden, #{e.message}, (response headers: #{e.response.headers})"
+        Rails.logger.warn e.response
+
         if (tries -= 1) > 0
           @token = get_new_token
           retry
@@ -29,18 +31,30 @@ module LimelightApi
     end
 
     def make_dir path
-      @make_dir_response = RestClient.post( @jsonrpc_endpoint, 
-        request_jsonrpc('makeDir2', {token: @token, path: path}) )
+      @make_dir_response = RestClient.post(@jsonrpc_endpoint,
+                                           request_jsonrpc('makeDir2', {token: @token, path: path}))
+    rescue RestClient::ExceptionWithResponse => e
+      Rails.logger.warn 'Limelight makeDir2 JSONRPC Request FAILED:'
+      Rails.logger.warn e.message
+      Rails.logger.warn e.response
     end
 
     def delete_file path
-      @delete_file_response = RestClient.post( @jsonrpc_endpoint, 
-        request_jsonrpc('deleteFile', {token: @token, path: path}) )
+      @delete_file_response = RestClient.post( @jsonrpc_endpoint,
+                                               request_jsonrpc('deleteFile', {token: @token, path: path}) )
+    rescue RestClient::ExceptionWithResponse => e
+      Rails.logger.warn 'Limelight deleteFile JSONRPC Request FAILED:'
+      Rails.logger.warn e.message
+      Rails.logger.warn e.response
     end
 
     def list_file path
-      @list_file_response = RestClient.post( @jsonrpc_endpoint, 
-        request_jsonrpc('listFile', {token: @token, path: path}) )
+      @list_file_response = RestClient.post( @jsonrpc_endpoint,
+                                             request_jsonrpc('listFile', {token: @token, path: path}) )
+    rescue RestClient::ExceptionWithResponse => e
+      Rails.logger.warn 'Limelight listFile JSONRPC Request FAILED:'
+      Rails.logger.warn e.message
+      Rails.logger.warn e.response
     end
 
     private
@@ -51,7 +65,7 @@ module LimelightApi
 
     def request_jsonrpc method, params, jsonrpc = '2.0'
       {
-        'id' => Time.now.to_f,
+        'id' => Time.now.to_f.to_s,
         'jsonrpc' => jsonrpc,
         'method' => method,
         'params' => params
@@ -59,7 +73,7 @@ module LimelightApi
     end
 
     def upload_headers file_base_name, directory
-      { 
+      {
         'X-Agile-Authorization' => @token,
         'X-Agile-Basename' => file_base_name,
         'X-Agile-Directory' => "#{directory}/"
@@ -67,7 +81,7 @@ module LimelightApi
     end
 
     def get_file file_path
-      File.open(file_path, "rb")
+      File.open(file_path, 'rb')
     end
   end
 end
